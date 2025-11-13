0fb28603e6f280e2081e57cc43e215b5d39d49dc
Georg Sieber <it@georg-sieber.de>
2025-11-13 15:27:08 +0100

    fix logging prefix

--- lib/Models/Log.class.php
+++ lib/Models/Log.class.php
@@ -29,16 +29,8 @@ class Log {
 		4 					=> '4 - No Logging',
 	];
 
-	public const ACTION_AGENT_API                   = 'oco.computer.agent';
-	public const ACTION_AGENT_API_RAW               = 'oco.computer.agent.api.rawrequest';
-	public const ACTION_AGENT_API_HELLO             = 'oco.computer.agent.hello';
-	public const ACTION_AGENT_API_UPDATE            = 'oco.computer.agent.update';
-	public const ACTION_AGENT_API_UPDATE_JOB_STATE  = 'oco.computer.agent.update_job_state';
-
-	public const ACTION_CLIENT_API_RAW = 'oco.client.api.rawrequest';
-	public const ACTION_CLIENT_API     = 'oco.client.api';
-	public const ACTION_CLIENT_WEB     = 'oco.client.web';
-
-	public const ACTION_SELF_SERVICE_WEB     = 'oco.self_service.web';
+	public const ACTION_CLIENT_API_RAW = 'fluentdb.client.api.rawrequest';
+	public const ACTION_CLIENT_API     = 'fluentdb.client.api';
+	public const ACTION_CLIENT_WEB     = 'fluentdb.client.web';
 
 }
